36981082d228812e65a36b90583c4c93d6ef776f
TeacherMJ <129888237+TeacherMJ@users.noreply.github.com>
2023-04-06 18:36:51 +0800

    Update jeepney.js

--- jeepney.js
+++ jeepney.js
@@ -1,1 +1,11 @@
-//filler file, mamaya ilalagay dito yung three.js code
+// filler file, mamaya ilalagay dito yung three.js code
+
+// angle ng camera
+
+// Light mula direkta sa taas, 90° mula x-axis
+
+// Texture ng jeep dito
+
+// Insert jeepney object mula sa clara.io
+
+// Render
